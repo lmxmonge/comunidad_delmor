308e25a7c91cffcaa5427315afac64f7316f4402
lmxmonge <programador1@delmor.com.ni>
2024-09-21 08:34:01 -0600

    feat: Mejorando drawer - Se agregaron mas items al drawer

--- lib/app/presentation/global_widgets/custom_drawer_controller/custom_drawer.dart
+++ lib/app/presentation/global_widgets/custom_drawer_controller/custom_drawer.dart
@@ -5,51 +5,151 @@ import '../../../../utils/iconos.dart';
 import 'custom_drawer_controller.dart';
 
 class CustomDrawer extends StatelessWidget {
-   const CustomDrawer({super.key});
+  const CustomDrawer({super.key});
 
   // final CustomDrawerController controller = Get.put(CustomDrawerController(),tag: "navbar",);
   @override
   Widget build(BuildContext context) {
-
-    return
-      GetBuilder<CustomDrawerController>(builder:(controller) {
+    return GetBuilder<CustomDrawerController>(
+      builder: (controller) {
         return Drawer(
-
           child: ListView(
-            padding: EdgeInsets.zero,
             children: [
-              const DrawerHeader(
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  border: Border.all(color: Colors.transparent),
+                  image: const DecorationImage(
+                    image: AssetImage(Iconos.drawerHeader),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Text('Drawer Header'),
+                child: null,
               ),
               listTileItem(
-                  iconPath: Iconos.user,
+                  iconPath: Iconos.userColor,
                   title: 'Perfil',
                   onTap: () {
                     controller.handleIndexChanged(0);
                   }),
-              listTileItem(title: "Cumpleaños", iconPath: Iconos.cake, onTap: () {
-                controller.handleIndexChanged(1);
-              }),
-              listTileItem(title: "Cerrar Sesión", iconPath: Iconos.cerrarSesion, onTap: (){
-                controller.cerrarSesion();
-              })
+              listTileItem(
+                  title: "Cumpleaños",
+                  iconPath: Iconos.cakeColor,
+                  onTap: () {
+                    controller.handleIndexChanged(1);
+                  }),
+              listTileItem(
+                  title: "Datos Laborales",
+                  iconPath: Iconos.maletinColor,
+                  onTap: () {}),
+              const Divider(),
+
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Documentos",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              listTileItem(
+                  title: "Circulares",
+                  iconPath: Iconos.catalogo,
+                  onTap: () {},
+                  hasNotification: true),
+              listTileItem(
+                  title: "Memorandums",
+                  iconPath: Iconos.memorandos,
+                  onTap: () {}),
+
+              listTileItem(
+                title: "Boletín Informativo",
+                iconPath: Iconos.boletin,
+                onTap: () {},
+                hasNotification: true,
+              ),
+
+              const Divider(),
+              listTileItem(
+                  title: "Saldo Comisariato",
+                  iconPath: Iconos.comisariato,
+                  onTap: () {
+                  }),
+
+              listTileItem(
+                  title: "Recorrido",
+                  iconPath: Iconos.recorrido,
+                  onTap: () {
+
+                  }),
+              const Divider(),
+              listTileItem(
+                  title: "Configuración",
+                  iconPath: Iconos.configuraciones,
+                  onTap: () {
+                  }),
+
+              listTileItem(
+                  title: "Cerrar Sesión",
+                  iconPath: Iconos.cerrarSesion,
+                  onTap: () {
+                    controller.cerrarSesion();
+                  }),
+
             ],
           ),
         );
-      }, );
+      },
+    );
   }
 }
 
-ListTile listTileItem(
-    {required String title,
-    required String? iconPath,
-    required Function onTap}) {
+ListTile listTileItem({
+  required String title,
+  required String? iconPath,
+  required Function onTap,
+  bool hasNotification = false,
+}) {
   return ListTile(
-    leading: Image.asset(iconPath!,
-        width: Tamanios.iconSizeDrawer, height: Tamanios.iconSizeDrawer),
+    leading: Stack(
+      alignment: Alignment.center,
+      children: [
+        const SizedBox(
+          width: Tamanios.iconSizeDrawer + 5,
+          height: Tamanios.iconSizeDrawer + 30,
+          // decoration:
+          // BoxDecoration(color: Colors.grey[200], shape: BoxShape.circle),
+        ),
+        Positioned(
+          left: 0,
+          child: Image.asset(iconPath!,
+              width: Tamanios.iconSizeDrawer, height: Tamanios.iconSizeDrawer),
+        ),
+        hasNotification
+            ? Positioned(
+                top: 10,
+                right: 0,
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: const BoxDecoration(
+                      color: Colors.red, shape: BoxShape.circle),
+                  child: const Center(
+                      child: Text(
+                    "2",
+                    style: TextStyle(color: Colors.white, fontSize: 8),
+                  )),
+                ),
+              )
+            : const SizedBox(
+                width: 0,
+                height: 0,
+              )
+      ],
+    ),
     title: Text(title),
     onTap: () {
       onTap();
--- lib/utils/iconos.dart
+++ lib/utils/iconos.dart
@@ -9,6 +9,20 @@ class Iconos {
   static const team = '${_path}team.png';
   static const maletin = '${_path}maletin.png';
   static const cerrarSesion = '${_path}cerrar-sesion-de-usuario.png';
+  static const maletinColor = '${_path}maletinColor.png';
+  static const cakeColor = '${_path}cakeColor.png';
+  static const userColor = '${_path}userColor.png';
+  static const catalogo = '${_path}catalogo.png';
+  static const memorandos = '${_path}memorandos.png';
+  static const boletin = '${_path}boletin-informativo.png';
+
+  static const configuraciones = '${_path}configuraciones.png';
+  static const comisariato = '${_path}comisariato.png';
+  static const recorrido = '${_path}recorrido.png';
+
+
+  static const String _pathImages = 'lib/assets/images/';
+  static const String drawerHeader = '${_pathImages}comunidad.png';
 
 
 }
